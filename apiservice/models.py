940828c7a18b765d2b0e3696d093e17f2b3c5a29
sucholbluk <sucholbiaklukasz@gmail.com>
2025-03-13 14:40:04 +0100

    db: create repository model

--- apiservice/models.py
+++ apiservice/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    """Model representing dataset repository"""
+
+    name = models.CharField(max_length=255)
+    id = models.CharField(max_length=255, primary_key=True)
+    doi = models.CharField(max_length=255, unique=True)
+    api_url = models.URLField()
+    last_update = models.DateField()
+
+    def __str__(self):
+        return self.name
